6ccf086aef74eea5cb06ca7949e93a01a423b342
Danila Egorov <c4nsero@mail.ru>
2022-04-07 10:25:10 +0300

    update postgres.go: minor fixes
    Обновил Json файл с данными, выкладывать его я конечно же не буду

--- postgres/postgres.go
+++ postgres/postgres.go
@@ -19,7 +19,7 @@ type Name struct {
 
 func InitData(db *sqlx.DB) error {
 
-	data := make([]byte, 6124971)
+	data := make([]byte, 6346950) //6113867 4429392
 	names := make([]Name, 25897)
 
 	file, err := os.Open("names.json")
